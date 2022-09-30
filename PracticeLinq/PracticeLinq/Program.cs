01a348159c23890d46cd93cec447de446c690873
cmreavis97 <cmreavis97@gmail.com>
2022-09-30 12:25:11 -0400

    First commit

---
+++ PracticeLinq/PracticeLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+List<string> vidyaGames = new()
+{
+    "Fallout",
+    "Call of Duty",
+    "Overwatch",
+    "Bloons",
+    "Halo",
+    "Elden Ring",
+    "Rocket League"
+};
+//sorting method with Linq, lambda operator, & method syntax
+vidyaGames = vidyaGames.OrderBy(x => x.Length).ToList();
+
+//foreach loop to print out each string beginning with shortest string
+
+/* for future reference, was stuck trying to simply print the list -
+you MUST iterate through the items, simple concept but easy to forget I guess! */
+
+foreach (string item in vidyaGames)
+Console.WriteLine(item);
+
+
